66016e6306795b0362b47b7948f5822d0113c84b
Zachary Neal <zpneal@msu.edu>
2022-05-31 21:20:13 +0000

    version 1.0.0

--- inst/doc/incidentally.R
+++ inst/doc/incidentally.R
@@ -7,6 +7,12 @@ library(igraph)
 oldpar <- par(mar = c(0, 0, 1, 0))
 par(mar = c(0, 0, 1, 0))
 
+## ---- echo = FALSE, results = FALSE-------------------------------------------
+I <- matrix(c(1,0,0,0,1,1,1,1,0,0,1,1,1,1,0),3,5)
+B <- graph_from_incidence_matrix(I)
+V(B)$name <- c("A","B","C","a","b","c","d","e")
+plot(B, layout = layout_as_bipartite(B))
+
 ## ----setup--------------------------------------------------------------------
 set.seed(5)
 library(incidentally)
@@ -14,7 +20,7 @@ library(incidentally)
 ## -----------------------------------------------------------------------------
 I <- incidence.from.probability(10, 10, .2)
 I
-mean(I)  #Fill rate/Density
+mean(I)  #Fill rate/density
 
 ## -----------------------------------------------------------------------------
 I <- incidence.from.probability(10, 10, .2, constrain = FALSE)
@@ -65,35 +71,42 @@ hist(colSums(I), main = "Column Marginals")
 
 ## ---- fig.show="hold", out.width="40%"----------------------------------------
 G <- erdos.renyi.game(15, .5)  #A random social network of 15 people, as igraph
-I <- incidence.from.adjacency(G, k = 3, p = .75, model = "team")  #Teams model
+I <- incidence.from.adjacency(G, k = 1, p = .75, model = "team")  #Teams model
 class(I)  #Incidence matrix returned as igraph object
 V(I)$shape <- ifelse(V(I)$type, "square", "circle")  #Add shapes
-plot(G, main="Social Network")
-plot(I, layout = layout_as_bipartite(I), main="New Teams")
+plot(G, main="Prior Team Collaborations")
+plot(I, layout = layout_as_bipartite(I), main="New Team")
 
 ## ---- fig.show="hold", out.width="40%"----------------------------------------
 G <- erdos.renyi.game(15, .33)  #A random social network of 15 people, as igraph
-I <- incidence.from.adjacency(G, k = 3, p = .75, model = "group")  #Groups model
+I <- incidence.from.adjacency(G, k = 1, p = .75, model = "group")  #Groups model
 V(I)$shape <- ifelse(V(I)$type, "square", "circle")  #Add shapes
 plot(G, main="Social Network")
-plot(I, layout = layout_as_bipartite(I), main="New Groups")
+plot(I, layout = layout_as_bipartite(I), main="New Group")
 
 ## ---- fig.show="hold", out.width="40%"----------------------------------------
-G <- erdos.renyi.game(15, .33)  #A random social network of 15 people, as igraph
-I <- incidence.from.adjacency(G, k = 3, d = 2, p = .90, model = "blau")  #Groups model
+G <- erdos.renyi.game(15, .5)  #A random social network of 15 people, as igraph
+I <- incidence.from.adjacency(G, k = 1, p = .95, model = "blau")  #Groups model
 V(I)$shape <- ifelse(V(I)$type, "square", "circle")  #Add shapes
 plot(G, layout = layout_with_mds(G), main="Social Network")
 plot(I, layout = layout_as_bipartite(I), main="New Organizations")
 
 ## -----------------------------------------------------------------------------
-I <- incidence.from.probability(10, 10, .3)
+I <- matrix(c(1,0,0,0,1,0,1,0,1),3,3)
 I
-rowSums(I)
-colSums(I)
-I <- add.blocks(I, blocks = 2, density = .8)
-I
-rowSums(I)
-colSums(I)
+curveball(I)
+
+## ---- echo = TRUE, results = 'hide'-------------------------------------------
+I <- incidence.from.probability(R = 10, C = 10, P = .2)
+blocked <- add.blocks(I, density = .75, 
+                       rowblock = c(1,1,1,1,1,2,2,2,2,2),
+                       colblock = c(1,1,1,1,1,2,2,2,2,2))
+
+## -----------------------------------------------------------------------------
+I  #Original matrix
+blocked  #Blocked matrix
+all(rowSums(I)==rowSums(blocked)) #Row marginals preserved
+all(colSums(I)==colSums(blocked)) #Column marginals preserved
 
 ## ---- echo = FALSE, message = FALSE-------------------------------------------
 par(oldpar) #Restore old parameters
