f6d90639059cb570ae14e343a45c3fe86b343a0b
Zachary Neal <zpneal@msu.edu>
2025-07-28 22:10:02 +0000

    version 1.0.3

--- inst/doc/congress.R
+++ inst/doc/congress.R
@@ -1,8 +1,8 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(collapse = TRUE, comment = "#>")
 knitr::opts_knit$set(global.par = TRUE)
 
-## ---- echo = FALSE, message = FALSE-------------------------------------------
+## ----echo = FALSE, message = FALSE--------------------------------------------
 library(igraph)
 oldmar <- par()$mar
 par(mar = c(0, 0, 1, 0))
@@ -10,7 +10,7 @@ par(mar = c(0, 0, 1, 0))
 ## -----------------------------------------------------------------------------
 library(incidentally)
 
-## ---- echo = TRUE, results = 'hide', warning = FALSE--------------------------
+## ----echo = TRUE, results = 'hide', warning = FALSE---------------------------
 I <- incidence.from.congress(session = 115, types = c("sres"), areas = c("All"), format = "data", narrative = TRUE)
 
 ## -----------------------------------------------------------------------------
@@ -18,7 +18,7 @@ I$matrix[1:5,1:5]
 I$legislator[1:5,1:5]
 I$bills[1:5,c(1,2,4,5)]
 
-## ---- echo = TRUE, results = 'hide', warning = FALSE--------------------------
+## ----echo = TRUE, results = 'hide', warning = FALSE---------------------------
 B <- incidence.from.congress(session = 115, types = c("sres"), areas = c("All"), format = "igraph")
 
 ## -----------------------------------------------------------------------------
@@ -32,23 +32,17 @@ network <- sdsm(B, alpha = 0.05, narrative = TRUE)
 network
 
 ## -----------------------------------------------------------------------------
-V(network)$color <- rgb(1,0,0,.5)  #Define the color of Republicans
-V(network)$color[which(V(network)$party=="D")] <- rgb(0,0,1,.5)  #...of Democrats
-V(network)$color[which(V(network)$party=="I")] <- rgb(0,1,0,.5)  #...of Independents
-plot(network, vertex.label = NA, vertex.color = V(network)$color, vertex.frame.color = NA, vertex.size = 10)
+plot(network, vertex.label = NA, vertex.frame.color = NA, vertex.size = 10)
 
 ## -----------------------------------------------------------------------------
 signed <- sdsm(B, alpha = 0.05, signed = TRUE)
 
 ## -----------------------------------------------------------------------------
-V(signed)$color <- rgb(1,0,0,.5)  #Define the color of Republicans
-V(signed)$color[which(V(signed)$party=="D")] <- rgb(0,0,1,.5)  #...of Democrats
-V(signed)$color[which(V(signed)$party=="I")] <- rgb(0,1,0,.5)  #...of Independents
 E(signed)$color <- rgb(0,1,0,1)  #Define color of positive edges
-E(signed)$color[which(E(signed)$weight==-1)] <- rgb(1,0,0,.01)  #Define color of negative edges
-layout <- layout_nicely(delete_edges(signed, which(E(signed)$weight==-1)))  #Get layout based on positive edges
-plot(signed, vertex.label = NA, vertex.color = V(signed)$color, vertex.frame.color = NA, vertex.size = 10, layout = layout)
+E(signed)$color[which(E(signed)$sign==-1)] <- rgb(1,0,0,.05)  #Define color of negative edges
+layout <- layout_nicely(delete_edges(signed, which(E(signed)$sign==-1)))  #Get layout based on positive edges
+plot(signed, vertex.label = NA, vertex.frame.color = NA, vertex.size = 10, layout = layout)
 
-## ---- echo = FALSE------------------------------------------------------------
+## ----echo = FALSE-------------------------------------------------------------
 par(mar = oldmar)
 
--- inst/doc/incidentally.R
+++ inst/doc/incidentally.R
@@ -1,13 +1,13 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(collapse = TRUE, comment = "#>")
 knitr::opts_knit$set(global.par = TRUE)
 
-## ---- echo = FALSE, message = FALSE-------------------------------------------
+## ----echo = FALSE, message = FALSE--------------------------------------------
 library(igraph)
 oldmar <- par()$mar
 par(mar = c(0, 0, 1, 0))
 
-## ---- echo = FALSE, results = FALSE-------------------------------------------
+## ----echo = FALSE, results = FALSE--------------------------------------------
 I <- matrix(c(1,0,0,0,1,1,1,1,0,0,1,1,1,1,0),3,5)
 B <- graph_from_incidence_matrix(I)
 V(B)$name <- c("A","B","C","a","b","c","d","e")
@@ -33,25 +33,25 @@ I
 rowSums(I)  #Row marginals
 colSums(I)  #Column marginals
 
-## ---- fig.show="hold", out.width="33%"----------------------------------------
+## ----fig.show="hold", out.width="33%"-----------------------------------------
 I <- incidence.from.distribution(R = 100, C = 100, P = 0.2,
   rowdist = c(1,1), coldist = c(1,1))
 hist(rowSums(I), main = "Row Marginals")
 hist(colSums(I), main = "Column Marginals")
 
-## ---- fig.show="hold", out.width="33%"----------------------------------------
+## ----fig.show="hold", out.width="33%"-----------------------------------------
 I <- incidence.from.distribution(R = 100, C = 100, P = 0.2,
   rowdist = c(1,10), coldist = c(1,10))
 hist(rowSums(I), main = "Row Marginals")
 hist(colSums(I), main = "Column Marginals")
 
-## ---- fig.show="hold", out.width="33%"----------------------------------------
+## ----fig.show="hold", out.width="33%"-----------------------------------------
 I <- incidence.from.distribution(R = 100, C = 100, P = 0.2,
   rowdist = c(10,1), coldist = c(10,1))
 hist(rowSums(I), main = "Row Marginals")
 hist(colSums(I), main = "Column Marginals")
 
-## ---- fig.show="hold", out.width="33%"----------------------------------------
+## ----fig.show="hold", out.width="33%"-----------------------------------------
 I <- incidence.from.distribution(R = 100, C = 100, P = 0.2,
   rowdist = c(10,10), coldist = c(10,10))
 hist(rowSums(I), main = "Row Marginals")
@@ -63,13 +63,13 @@ I <- incidence.from.distribution(R = 100, C = 100, P = 0.2,
 rowSums(I)
 colSums(I)
 
-## ---- fig.show="hold", out.width="33%"----------------------------------------
+## ----fig.show="hold", out.width="33%"-----------------------------------------
 I <- incidence.from.distribution(R = 100, C = 100, P = 0.2,
   rowdist = c(1,10), coldist = c(10,1))
 hist(rowSums(I), main = "Row Marginals")
 hist(colSums(I), main = "Column Marginals")
 
-## ---- fig.show="hold", out.width="40%"----------------------------------------
+## ----fig.show="hold", out.width="40%"-----------------------------------------
 G <- erdos.renyi.game(15, .5)  #A random social network of 15 people, as igraph
 I <- incidence.from.adjacency(G, k = 1, p = .75, model = "team")  #Teams model
 class(I)  #Incidence matrix returned as igraph object
@@ -77,14 +77,14 @@ V(I)$shape <- ifelse(V(I)$type, "square", "circle")  #Add shapes
 plot(G, main="Prior Team Collaborations")
 plot(I, layout = layout_as_bipartite(I), main="New Team")
 
-## ---- fig.show="hold", out.width="40%"----------------------------------------
+## ----fig.show="hold", out.width="40%"-----------------------------------------
 G <- erdos.renyi.game(15, .33)  #A random social network of 15 people, as igraph
 I <- incidence.from.adjacency(G, k = 1, p = .75, model = "club")  #Groups model
 V(I)$shape <- ifelse(V(I)$type, "square", "circle")  #Add shapes
 plot(G, main="Social Network")
 plot(I, layout = layout_as_bipartite(I), main="New Group")
 
-## ---- fig.show="hold", out.width="40%"----------------------------------------
+## ----fig.show="hold", out.width="40%"-----------------------------------------
 G <- erdos.renyi.game(15, .5)  #A random social network of 15 people, as igraph
 I <- incidence.from.adjacency(G, k = 1, p = .95, model = "org")  #Groups model
 V(I)$shape <- ifelse(V(I)$type, "square", "circle")  #Add shapes
@@ -96,7 +96,12 @@ I <- matrix(c(1,0,0,0,1,0,1,0,1),3,3)
 I
 curveball(I)
 
-## ---- echo = TRUE, results = 'hide'-------------------------------------------
+## -----------------------------------------------------------------------------
+space <- space(R = c(2,1,1), C = c(1,1,2))
+space$matrices
+space$perturbations
+
+## ----echo = TRUE, results = 'hide'--------------------------------------------
 I <- incidence.from.probability(R = 10, C = 10, P = .2)
 blocked <- add.blocks(I, density = .75, 
                        rowblock = c(1,1,1,1,1,2,2,2,2,2),
@@ -108,6 +113,6 @@ blocked  #Blocked matrix
 all(rowSums(I)==rowSums(blocked)) #Row marginals preserved
 all(colSums(I)==colSums(blocked)) #Column marginals preserved
 
-## ---- echo = FALSE------------------------------------------------------------
+## ----echo = FALSE-------------------------------------------------------------
 par(mar = oldmar)
 
